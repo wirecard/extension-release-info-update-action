3dfac819df1fc732d673d3390aed31aa768b520b
tatjana.starcenko <tatjana.starcenko@wirecard.com>
2020-05-17 15:09:38 +0200

    Add pydoc

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,11 @@ FROM python:3
 
 RUN pip install lastversion gitpython pyyml markdown bs4 html2markdown
 
+#copy config files to the same folder as scripts
 COPY *.json /usr/bin/
 COPY . /usr/bin/
 COPY entrypoint.sh /entrypoint.sh
+#TODO remove test_data
+
 
 ENTRYPOINT ["/entrypoint.sh"]
--- src/InternalFileUpdater.py
+++ src/InternalFileUpdater.py
@@ -52,13 +52,20 @@ class InternalFileUpdater(FileUpdater):
                     self.platform_compatibility_versions) > 1 else self.platform_compatibility_versions[0])
 
     def update_files(self):
+        """
+        Updates all internal files from the internal-files.json
+        """
         shop_extension_file_list = FileActionHelper.get_data_from_internal_files()[self.extension]
         for file_name, replace_hint_entries in shop_extension_file_list.items():
             file_lines = self.get_updated_file_lines(file_name, replace_hint_entries)
             with open(FileActionHelper.get_file_path(file_name), 'w') as internal_file:
                 internal_file.writelines(file_lines)
 
-    def get_updated_file_lines(self, file_name, replace_hint_entries):
+    def get_updated_file_lines(self, file_name, replace_hint_entries) -> list:
+        """
+        Returns updated file lines for file
+        :return: list
+        """
         with open(FileActionHelper.get_file_path(file_name), 'r') as internal_file:
             file_lines = internal_file.readlines()
             if Constants.README_FILE in file_name:
@@ -67,14 +74,11 @@ class InternalFileUpdater(FileUpdater):
                 file_lines = self.get_updated_internal_file_lines(file_lines, replace_hint_entries)
         return file_lines
 
-    # def get_file_line_containing_hint_index(self, file_lines, hint):
-    #     for line in file_lines:
-    #         if hint in line:
-    #             return file_lines.index(line)
-    #     print("Can't find {}, the value will not be updated".format(hint))
-    #     return None
-
-    def get_updated_internal_file_lines(self, file_lines, hint_entries):
+    def get_updated_internal_file_lines(self, file_lines, hint_entries) -> list:
+        """
+        Returns updated file lines for internal file
+        :return: list
+        """
         new_file_lines = file_lines
         for replace_field, hint in hint_entries.items():
             if Constants.INTERNAL_CHANGELOG_ENTRY_NAME in hint:
@@ -92,7 +96,11 @@ class InternalFileUpdater(FileUpdater):
         # after string "== Changelog ==" add new release version and changelog commments
         return file_lines
 
-    def get_updated_readme_badges(self, file_lines, hint_entries):
+    def get_updated_readme_badges(self, file_lines, hint_entries) -> list:
+        """
+        Returns file lines with updated readme badges
+        :return: list
+        """
         new_file_lines = file_lines
         for replace_field, hint in hint_entries.items():
             if Constants.PHP_IN_README_ENTRY_NAME in replace_field:
@@ -101,7 +109,11 @@ class InternalFileUpdater(FileUpdater):
                 new_file_lines = self.get_lines_with_replaced_badges(new_file_lines, hint, replace_field)
         return new_file_lines
 
-    def get_lines_with_replaced_php_badges(self, file_lines, hint):
+    def get_lines_with_replaced_php_badges(self, file_lines, hint) -> list:
+        """
+        Returns file lines updated php readme badges
+        :return: list
+        """
         php_index = FileUpdater.get_index_of_first_list_entry_containing_text(file_lines, hint)
         php_sample_line = file_lines[php_index]
         file_lines_without_php_badges = [x for x in file_lines if hint not in x]
@@ -110,7 +122,11 @@ class InternalFileUpdater(FileUpdater):
             new_file_lines.insert(php_index, re.sub('\d.\d', php_version, php_sample_line))
         return new_file_lines
 
-    def get_lines_with_replaced_badges(self, file_lines, hint, replace_hint):
+    def get_lines_with_replaced_badges(self, file_lines, hint, replace_hint) -> list:
+        """
+        Returns file lines with updated badges (except php)
+        :return: list
+        """
         replace_index = FileUpdater.get_index_of_first_list_entry_containing_text(file_lines, hint)
         new_line = self.internal_files_entry_to_output_info_dict[replace_hint]
         substitute = re.search('\d\.\d\.\d', file_lines[replace_index])
@@ -120,6 +136,3 @@ class InternalFileUpdater(FileUpdater):
             file_lines[replace_index] = re.sub('\d\.\d', new_line, file_lines[replace_index])
         return file_lines
 
-updater = InternalFileUpdater('woocommerce', '3.2.2', '3.2.1', ['7.1', '7.2'],
-                              ['7.2'], ['3.3.6', '3.9.0'], ['3.8.0'], ['1.1.1'], ['1.1.1', '2.2.2'])
-updater.update_files()
